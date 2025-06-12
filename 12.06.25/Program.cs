657eddb4af9aca5992f19f31367dfc283feded05
Denislav-I7 <denislavivanov873@gmail.com>
2025-06-12 10:41:25 +0300

    Add files via upload

---
+++ 12.06.25/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _12._06._25
+{
+    internal class Program
+    {
+        public class BankAcount()
+        {
+            public int Id;
+            public double balance;
+
+            public int ID
+            {
+                get { return Id; }
+                set { Id = value; }
+            }
+
+            public void Deposit(double amount)
+            {
+                balance = balance + amount;
+            }
+
+            public void Withdraw(double amount)
+            {
+                if (amount <= balance)
+                {
+                    balance = balance - amount;
+                }
+                else
+                {
+                    Console.WriteLine("Not enough money");
+                }
+            }
+
+            public override string ToString()
+            {
+                return $"ID:{Id},Balance:{balance:F2}";
+            }
+
+
+            static void Main(string[] args)
+            {
+                BankAcount acount = new BankAcount();
+                acount.Id = 1;
+                acount.Deposit(15);
+                acount.Withdraw(5);
+
+                Console.WriteLine(acount.ToString());
+            }
+        }
+    }
+}
